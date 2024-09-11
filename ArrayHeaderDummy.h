4cc47c3a5099c387af501eced036be52ef8f2e2c
Yme <yghuang@mails.ccnu.edu.cn>
2024-09-11 22:19:47 +0200

    v1.0: A dummy array header, change the name and data in real use

---
+++ ArrayHeaderDummy.h
@@ -0,0 +1,32 @@
+#ifndef __Datapts_arry__
+#define __Datapts_arry__
+
+namespace mArgs {
+    const int n_Enrpts = 8;
+    float sNN_RHIC[n_Enrpts] = {3.0, 7.7, 9.2, 11.5, 14.6, 17.3, 19.6, 27};
+
+
+    float C42_data_cen5_sNN[n_Enrpts] = {0};
+    float C42_stat_cen5_sNN[n_Enrpts] = {0};
+    float C42_sys_cen5_sNN[n_Enrpts] = {0};
+
+
+    ////================
+
+    float K21_p_data_cen5_sNN[n_Enrpts] = {0};
+    float K21_p_stat_cen5_sNN[n_Enrpts] = {0};
+    float K21_p_sys_cen5_sNN[n_Enrpts] = {0};
+
+
+    float K31_p_data_cen5_sNN[n_Enrpts] = {0};
+    float K31_p_stat_cen5_sNN[n_Enrpts] = {0};
+    float K31_p_sys_cen5_sNN[n_Enrpts] = {0};
+
+
+    float K41_p_data_cen5_sNN[n_Enrpts] = {0};
+    float K41_p_stat_cen5_sNN[n_Enrpts] = {0};
+    float K41_p_sys_cen5_sNN[n_Enrpts] = {0};
+}
+
+
+#endif
